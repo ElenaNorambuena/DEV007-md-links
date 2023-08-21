1e255b5630c4150ceafe03948c8d79385a88dbdb
ElenaNorambuena <elena.norambuena.borquez@gmail.com>
2023-08-21 16:37:36 -0400

    mdfunction

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-const fs = require('fs'); // Se importa el módulo file system de Node.js
+const fs = require('fs');// módulo filesystem
+const path = require('path'); // módulo path para trabajar rutas y directorios
+const marked = require('marked'); // paquete marked para convertir de markdown a html
 
-const path = require('path'); // Se importa el módulo 'path' de Node.js (para trabajar con rutas y directorios, en este caso rutas)
+const mdLinks = (filePath) => {// exporta una función anónima como el valor de 'module.exports'para que otros archivos lo importen.
 
-const marked = require('marked'); // Se importa paquete 'marked' para convertir Markdown a HTML.
+  return new Promise((resolve, reject) => {
+    const absolutePath = path.resolve(filePath);// de relativa a absoluta
 
-const mdLinks= (filePath) => { // Se exporta una función anónima como el valor de 'module.exports', lo que permitirá que otros archivos lo importen.
+    // Verificar si el archivo tiene extensión .md
+    if (path.extname(absolutePath) !== '.md') {
+      reject(new Error('El archivo debe ser de formato .md'));
+      return; // Salir de la función si no es un archivo .md
+    }
 
-  return new Promise((resolve, reject) => { // Nueva Promesa.
+    try {
+      const result = fs.readFileSync(absolutePath, 'utf8');// leyendo contenido UTF-8 usando promesas de módulo 'fs'
+      console.log(result);
+      const links = [];// array vacío para links encontrados
 
-    const absolutePath = path.resolve(filePath); // Convertimos la ruta relativa en absoluta con módulo 'path'.
+      const renderer = new marked.Renderer();// instancia del renderizador personalizado proporcionado por el paquete 'marked'
 
-   const result= fs.readFileSync(absolutePath, 'utf8') // Se lee contenido del archivo en formato UTF-8 usando promesas de módulo 'fs'.
-    console.log(result);
-        const links = []; // Creamos un arreglo vacío para almacenar los enlaces encontrados.
+      renderer+9.link = (href, title, text) => {
+        links.push({ href, text, file: absolutePath });
+      };
 
-        const renderer = new marked.Renderer(); // Creamos una instancia del renderizador personalizado proporcionado por el paquete 'marked'.
+      marked(result, { renderer });
 
-        renderer.link = (href, title, text) => { // Sobreescribimos el método 'link' del renderizador para extraer y almacenar los enlaces encontrados.
-          links.push({ href, text, file: absolutePath }); // Agregamos un objeto al arreglo 'links' con la URL (href), el texto y la ruta del archivo.
-        };
-
-        marked(result, { renderer }); // Utilizamos el renderizador personalizado para analizar el contenido Markdown y extraer los enlaces.
-
-        if (links.length === 0) { // Si no se encontraron enlaces en el archivo, rechazamos la promesa con un error.
-          reject(new Error('No se encontraron enlaces en el archivo.'));
-        } else { // Si se encontraron enlaces, resolvemos la promesa con el arreglo de enlaces.
-          resolve(links);
-        }
+      if (links.length === 0) {
+        reject(new Error('No se encontraron enlaces en el archivo.'));
+      } else {
+        resolve(links);
+      }
+    } catch (error) {
+      reject(error); // Manejar errores al leer el archivo
+    }
   });
 };
+
 mdLinks('./README.md')
