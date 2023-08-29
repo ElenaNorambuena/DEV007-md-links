30b7ed6989adf84e0266cb40b94014d27a211e73
ElenaNorambuena <elena.norambuena.borquez@gmail.com>
2023-08-29 14:19:51 -0400

    objeto listo

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const path = require('path'); // módulo path para trabajar rutas y directorios
 const marked = require('marked'); // paquete marked para convertir de markdown a html
 const cheerio = require('cheerio'); // importando cheerio
 const url = require('url'); // módulo url para resolver rutas
+const axios = require('axios'); // importando axios
 
-const mdLinks = (filePath) => {// exporta una función anónima como el valor de 'module.exports'para que otros archivos lo importen.
+// Definir la función mdLinks con un parámetro opcional validate
+const mdLinks = (filePath, validate = false) => {// exporta una función anónima como el valor de 'module.exports'para que otros archivos lo importen.
 
   return new Promise((resolve, reject) => {
     const absolutePath = path.resolve(filePath);// de relativa a absoluta
+    const dirName = path.dirname(absolutePath); // Obtener el directorio del archivo
+    console.log(dirName); // Imprimir el directorio en la consola
 
     // Verificar si el archivo tiene extensión .md
     if (path.extname(absolutePath) !== '.md') {
@@ -22,36 +26,65 @@ const mdLinks = (filePath) => {// exporta una función anónima como el valor de
       const html = marked.parse(result);
       console.log(html);
 
-// Carga el contenido HTML en Cheerio
-const $ = cheerio.load(html);
-// Selecciona todos los elementos 'a' que tienen el atributo 'href' y extrae sus valores
-const links = [];
-$('a[href]').each((index, element) => {
-    // Resolver la ruta relativa con respecto a la ruta base del archivo
-    const href = url.resolve(absolutePath, $(element).attr('href'));
-    // Obtener el texto del elemento
-    const text = $(element).text();
-    // Crear un objeto con las propiedades url y text
-    const link = {url: href, text: text};
-    // Añadir el objeto al array de links
-    links.push(link);
-});
-console.log(links);
+      // Carga el contenido HTML en Cheerio
+      const $ = cheerio.load(html);
+      // Selecciona todos los elementos 'a' que tienen el atributo 'href' y extrae sus valores
+      const links = [];
+      $('a[href]').each((_, element) => {
+          // Resolver la ruta relativa con respecto a la ruta base del archivo
+          const href = url.resolve(absolutePath, $(element).attr('href'));
+          // Obtener el texto del elemento
+          const text = $(element).text();
+          // Crear un objeto con las propiedades url y text
+          const link = { href, text};
+          // Añadir el objeto al array de links
+          links.push(link);
+      });
+      console.log(links);
 
-// Usar el método map para transformar el array de links en un array de objetos
-const objects = links.map(link => {
-    // Devolver un nuevo objeto con las propiedades url y text
-    return {url: link.url, text: link.text};
-});
-console.log(objects);
+      // verifico si validate es true
+      if (validate) {
+        // map para transformar array de links en un array de promesas
+        const promises = links.map(link => {
+          // petición HTTP al enlace
+          return axios.get(link.href)
+            .then(response => {
+              // Si la petición tiene éxito, devolver un objeto con las propiedades href, text, file, status y ok
+              return {
+                href: link.href,
+                text: link.text,
+                file: dirName,
+                status: response.status,
+                ok: response.statusText
+              };
+            })
+            .catch(error => {
+              // Si falla, devuelve un objeto con las propiedades href, text, file y message
+              return {
+                href: link.href,
+                text: link.text,
+                file: dirName,
+                message: error.message
+              };
+            });
+        });
 
-
-      // Resolver la promesa con el array de links
-      resolve(links);
+        // Usar Promise.all para esperar a que todas las promesas se resuelvan y devolver un nuevo array de objetos
+        return Promise.all(promises)
+          .then(objects => {
+            console.log(objects);
+            resolve(objects); // resuelve la promesa con el array de objetos validados
+          })
+          .catch(error => {
+            reject(error); // maneja errores al validar los enlaces
+          });
+      } else {
+        // si validate es false o undefined, devuelve array de links sin validar
+        resolve(links); // resuelve la promesa con el array de links sin validar
+      }
     } catch (error) {
-      reject(error); // Manejar errores al leer el archivo
+      reject(error);
     }
   });
-}; 
-
-mdLinks('./README.md')
+};
+mdLinks('./README.md', true)
